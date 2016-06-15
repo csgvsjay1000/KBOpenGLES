fa03babf27ac2d3a0293d8e7faee21e25c3e0eb4
csgvsjay1000 <254633397@qq.com>
2016-06-15 21:29:44 +0800

    tt

---
+++ KBOpenGLES/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KBOpenGLES
+//
+//  Created by feng on 16/6/15.
+//  Copyright © 2016年 feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KBOpenGLES/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KBOpenGLES
+//
+//  Created by feng on 16/6/15.
+//  Copyright © 2016年 feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
