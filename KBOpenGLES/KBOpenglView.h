26c83e18edbd949881d10b12deb1216453b31c5e
kobi_2016 <254633397@qq.com>
2016-06-21 19:02:18 +0800

    rtyrt

---
+++ KBOpenGLES/KBOpenglView.h
@@ -0,0 +1,23 @@
+//
+//  KBOpenglView.h
+//  KBOpenGLES
+//
+//  Created by chengshenggen on 6/21/16.
+//  Copyright © 2016 feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreMotion/CoreMotion.h>
+
+@interface KBOpenglView : UIView
+
+@property(nonatomic,strong) CMAttitude *referenceAttitude;
+@property(nonatomic,strong) CMMotionManager *motionManager;
+
+-(GLuint)rendImage:(UIImage *)image;
+
+- (void)newFrameReadyAtTime:(GLuint)texture;
+
+-(void)refreshFrame;
+
+@end
--- KBOpenGLES/ViewController.m
+++ KBOpenGLES/ViewController.m
@@ -7,8 +7,19 @@
 //
 
 #import "ViewController.h"
+#import "KBOpenglView.h"
 
-@interface ViewController ()
+
+@interface ViewController (){
+    KBOpenglView *glView;
+    KBOpenglView *glView2;
+    CADisplayLink *displayLink;
+    GLuint texture;
+    GLuint texture2;
+}
+
+@property(nonatomic,strong) CMAttitude *referenceAttitude;
+@property(nonatomic,strong) CMMotionManager *motionManager;
 
 @end
 
@@ -17,11 +28,80 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    glView2 = [[KBOpenglView alloc] initWithFrame:CGRectZero];
+
+    [self.view addSubview:glView2];
+
+    texture2 = [glView2 rendImage:[UIImage imageNamed:@"1234.jpg"]];
+
+    displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(displayLinkPresent)];
+    [displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    displayLink.paused = YES;
+    displayLink.frameInterval = 2;
+    glView = [[KBOpenglView alloc] initWithFrame:CGRectZero];
+    [self.view addSubview:glView];
+    texture = [glView rendImage:[UIImage imageNamed:@"1234.jpg"]];
+
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations{
+    return UIInterfaceOrientationMaskLandscape;
+}
+
+-(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    
+    [self startDeviceMotion];
+    
+    CGFloat width = self.view.frame.size.width;
+    CGFloat height = self.view.frame.size.height;
+    
+    glView.frame = CGRectMake(0, 0, width / 2 - 1, height);
+    glView2.frame = CGRectMake(width / 2 + 2, 0, width / 2 - 1, height);
+
+    [glView refreshFrame];
+    [glView2 refreshFrame];
+
+    displayLink.paused = NO;
+
+    
+}
+
+-(void)displayLinkPresent{
+    [glView newFrameReadyAtTime:texture];
+    [glView2 newFrameReadyAtTime:texture2];
+
+}
+
+#pragma mark - private methods
+- (void)startDeviceMotion {
+    
+    _motionManager = [[CMMotionManager alloc] init];
+    _referenceAttitude = nil;
+    _motionManager.deviceMotionUpdateInterval = 1.0 / 60.0;
+    _motionManager.gyroUpdateInterval = 1.0f / 60;
+    _motionManager.showsDeviceMovementDisplay = YES;
+    
+    [_motionManager startDeviceMotionUpdatesUsingReferenceFrame:CMAttitudeReferenceFrameXArbitraryCorrectedZVertical];
+    
+    _referenceAttitude = _motionManager.deviceMotion.attitude; // Maybe nil actually. reset it
+    
+    glView.motionManager = _motionManager;
+    glView.referenceAttitude = _referenceAttitude;
+
+    glView2.motionManager = _motionManager;
+    glView2.referenceAttitude = _referenceAttitude;
+}
+
+-(void)stopDeviceMotion{
+    [_motionManager stopDeviceMotionUpdates];
+    _referenceAttitude = nil;
 }
 
 @end
